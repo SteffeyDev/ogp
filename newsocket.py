e7aabb1121e44329f82ea983f967fd905c8aa4cf
SteffeyDev <steffeydev@icloud.com>
2015-02-12 22:08:22 -0600

    Merge branch 'OGP-LAB' of https://github.com/opengimbal/ogp into LYHS-FunMP
    Conflicts:
    newsocket.py

--- newsocket.py
+++ newsocket.py
@@ -48,6 +48,10 @@ stat = "ogp"
 s.write('3')
 s.write('8')
 
+<<<<<<< HEAD:newsocket.py
+=======
+
+>>>>>>> FETCH_HEAD:newsocket.py
 sqx = int(272)
 sqy = int(144)
 
@@ -426,3 +430,7 @@ if __name__ == "__main__":                       ##    since this is the main mo
     http_server = tornado.httpserver.HTTPServer(application)      ## opens the socket
     http_server.listen(8888)                                     ## on this port
     tornado.ioloop.IOLoop.instance().start()        ##   this starts the threaded socket loop
+<<<<<<< HEAD:newsocket.py
+=======
+        
+>>>>>>> FETCH_HEAD:newsocket.py
