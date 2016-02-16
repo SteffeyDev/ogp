7a0d78d5c6a1f6a62e565ff9453606f10677ddd6
SteffeyDev <steffeydev@icloud.com>
2016-02-16 11:56:37 -0500

    Fully commented so class

--- ogplab.py
+++ ogplab.py
@@ -16,6 +16,7 @@ darkpixels = 0
 l = int(4)
 s = serial.Serial('/dev/ttyUSB0', 9600)
 
+# Seek Object
 class so(object):
 
     def __init__(self, side, m, js, wsh, wsh2, c2, cam_mode):
@@ -26,7 +27,7 @@ class so(object):
         self.wsh = wsh # tornado.websocket.WebSocketHandler
         self.wsh2 = wsh2 # newSocket.py
         self.m = m # self.m is stepsize
-        self.l = side # self.l is mapsize
+        self.l = side # self.l is mapsize (starts as 8, plus/minus infinity)
         self.x = 0
         self.y = 0
         self.w = 0
@@ -35,9 +36,9 @@ class so(object):
         #l = self.l
         #x = -1
         #y = -1
-        self.countdownA = int(self.l)
-        self.countdownB = int(self.l)
-        self.countdownC = -1
+        self.countdownA = int(self.l) #countdownA is initially mapsize
+        self.countdownB = int(self.l) #countdownB is initially mapsize
+        self.countdownC = -1 #countdownC is initially -1
 
     def histo(self):
         cam_mode = self.cam_mode
@@ -53,16 +54,12 @@ class so(object):
 
         if cam_mode == 3:
             img1 = c2.getImage()
-        if cam_mode==1:
+        if cam_mode==1 or cam_mode==2:
             with picamera.PiCamera() as camera:
                 camera.resolution = (544, 288)
                 camera.capture('imagesmall.jpg')
             img1 = Image('imagesmall.jpg')
-        if cam_mode==2:
-            with picamera.PiCamera() as camera:
-                camera.resolution = (544, 288)
-                camera.capture('imagesmall.jpg')
-            img1 = Image('imagesmall.jpg')
+
         self.img1 = img1
 
         #SimpleCV function
@@ -80,12 +77,14 @@ class so(object):
             rgb1 = blobs[-1].meanColor()
             cent = blobs[-1].centroid()
 
+            #save the main image
             pth1 = "/var/www/images/image"
             pth3 = ".png"
             pth = pth1 + str(p) + pth3
             print pth
             img1.save(pth)
 
+            #save temporary scaled image
             thumbnail = img1.crop(150,25,250,250)
             thumbnail = thumbnail.scale(20,20)
             thumb1 = "/var/www/images/thumbs/thumb"
@@ -109,6 +108,8 @@ class so(object):
             rcolor=int(rcolor)
             gcolor=int(gcolor)
             bcolor=int(bcolor)
+
+            #pass back to app.js through newSocket.py
             wsh.write_message(wsh2, "rgb_" + str(rcolor)+"_"+str(gcolor)+"_"+str(bcolor))
             wsh.write_message(wsh2, "x_" + str(centroidx)+"_"+str(centroidy))
             img1.save(js.framebuffer)
@@ -117,6 +118,8 @@ class so(object):
         else:
             wshx = str(self.x)
             wshy = str(self.y)
+
+            #pass back to app.js
             wsh.write_message(wsh2, wshx + " " + wshy + "dark")
 
             print "dark"
@@ -129,18 +132,38 @@ class so(object):
         acd = int(1)
         acl = int(1)
         acr = int(1)
-        countdownA = self.countdownA
-        countdownB = self.countdownB
-        countdownC = self.countdownC
+        countdownA = self.countdownA #default 8
+        countdownB = self.countdownB #default 8
+        countdownC = self.countdownC #default -1
         ms = self.ms
         x = self.x
         y = self.y
-        if countdownA > 0:
 
+        # Process:
+        # 1 - countdownB is decremented from mapsize to 1 through Block 4 being run successively and exclusively
+        # 2 - Block 3 is run only once per decrement in A, sets countdownC to be the mapsize and countdownB to -1 to prevent Blocks 3 & 4 from being run in the future; countdownA is Decremented
+        # 3 - countdownC is then decremented to 1 through Block 2 being run successively and exclusively
+        # 4 - countdownC is set to -1 and countdownB is set to mapsize; causes to go back to step 1
+        # 5 - Steps 1-4 repeated until countdownA == 0, so the number of repetitions of cycle is the mapsize, and the number of repetitions per cycle is the mapsize*2 -> Therefore run (2*(mapsize^2)) times
+        # 6 - Once done running loops (countdownA == 0), sends message "map complete" to app.js
 
-            if countdownC == 1:
-                countdownB = int(self.l)
-                countdownC = -1
+        # Purpose:
+        # Creates Square Grid of size (mapsize x mapsize) - Starts in top left cell
+        # Block 4 moves to the right until reaches edge of grid
+        # Then Block 3 moves down one row
+        # Then Block 2 moves left unitl reaches edge
+        # Then Block one moves down one row
+        # Process repeates until reaches bottom of grid
+
+        # Master Block
+        if countdownA > 0: #first: yes
+
+            # Block 1
+            if countdownC == 1: # Only run once per cycle
+                countdownB = int(self.l) #mapsize again
+                countdownC = -1 #reset back to initial value
+
+                # move down one row in grid
                 print "down"
                 d = 'd'
                 s.write('9')
@@ -150,14 +173,20 @@ class so(object):
                 self.y = y
                 elf = self.histo()
 ##                time.sleep(1)
-                countdownA -=1
+                countdownA -=1 # decreases, once 0 prevents all blocks in here from running
                 wsh.write_message(wsh2, "m" )
+
+                # save updated values persistently
                 self.countdownA = countdownA
                 self.countdownB = countdownB
                 self.countdownC = countdownC
-            if countdownC > 1:
+
+            # Block 2
+            if countdownC > 1: #first: no
+
+                # move left one cell in grid
                 print "left"
-                countdownC -= 1
+                countdownC -= 1 # if above one, keep decreasing until it is 1
                 d = 'l'
                 s.write('2')
                 mov = acx(s, d, ms, acu, acd, acl, acr)
@@ -168,15 +197,21 @@ class so(object):
                 elf = self.histo()
                 s.write('s')
          ##       time.sleep(1)
+
+                # askes web interface if should continue mapping
                 wsh.write_message(wsh2, "m" )
 
+                # Save updated values persistently
                 self.countdownA = countdownA
                 self.countdownB = countdownB
                 self.countdownC = countdownC
 
-            if countdownB == 1:
-                countdownC = int(self.l)
-                countdownB = -1
+            # Block 3
+            if countdownB == 1: # Only run once per cycle
+                countdownC = int(self.l) # Set to mapsize: initially 8
+                countdownB = -1 # Prevent blocks 3 & 4 from being called again in this cycle
+
+                # move down one row in grid
                 print "down"
                 d = 'd'
                 s.write('9')
@@ -187,8 +222,12 @@ class so(object):
                 time.sleep(1)
                 elf = self.histo()
         ##        time.sleep(1)
-                countdownA -=1
+                countdownA -=1  # Decrements, once 0 prevents all blocks in here from running
+
+                # askes web interface if should continue mapping
                 wsh.write_message(wsh2, "m" )
+
+                # Save updated values persistently
                 self.countdownA = countdownA
                 self.countdownB = countdownB
                 self.countdownC = countdownC
@@ -196,9 +235,13 @@ class so(object):
                 print self.l
                 print countdownC
                 print self.countdownC
-            if countdownB > 1:
+
+            #Block 4
+            if countdownB > 1: #first: yes
+
+                # move right one row in grid
                 print "right"
-                countdownB-=1
+                countdownB-=1 # decreas B until it is 1
                 d = 'r'
                 s.write('4')
                 mov = acx(s, d, ms, acu, acd, acl, acr)
@@ -210,8 +253,11 @@ class so(object):
           ##      time.sleep(1)
                 elf = self.histo()
             ##    time.sleep(1)
+
+                # askes web interface if should continue mapping
                 wsh.write_message(wsh2, "m" )
 
+                # Save updated values persistently
                 self.countdownA = countdownA
                 self.countdownB = countdownB
                 self.countdownC = countdownC
@@ -223,10 +269,13 @@ class so(object):
             wsh2 = self.wsh2
 
          ##   wsh.write_message(wsh2, str(mySet) )
+
+            # tells app.js that map is done; app.js doens't actually do anything with this, but because an m wasn't sent this method is not called again
             wsh.write_message(wsh2, "map complete" )
 
             print "done"
 
+# not called from newsocket
 class autocal(object):
     def __init__(self, js, wsh, wsh2):
         self.js = js
@@ -362,7 +411,7 @@ class autocal(object):
         time.sleep(2)
         wsh.write_message(wsh2, "x_" + str(cal1) + "_" + str(cal2) + "_" + str(cal3)+ "_" + str(cal4) )
 
-
+# never called
 class hud2(object):
     def __init__(self, img1, js, stat, x, y, z):
         self.img1 = img1
@@ -407,12 +456,15 @@ class hud2(object):
         img1.drawText(str(rgb1), 10, 270, color=(255,255,255), fontsize=15)
         img1.save(js.framebuffer)
 
+# called to stop movement after designated amount of time
 class acx(object):
     def __init__(self, s, d, ms, acu, acd, acl, acr):
 
         self.s = s
         self.d = d
         self.ms = ms
+
+        # Following hardcoded to 1 when called from so
         self.acu = acu
         self.acd = acd
         self.acl = acl
