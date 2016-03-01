82b95856f3aa208774d2953d2fab0108a4880154
Peter Steffey <petersteffey@MasterPeters-Mac-mini.local>
2016-03-01 11:56:29 -0500

    Updates to install script

--- README.md
+++ README.md
@@ -10,8 +10,11 @@ an application of open-source and over-the-counter technologies that can turn an
 This fork is designed to totally redesign the User Experience, making it easier to user, understand, access, and control.  Watch for changes over the next few months.
 
 Quick install commands:
+sudo raspi-config
+  * Enable camera
+  * Expand filesystem (if new image)
 sudo apt-get install git -y
-sudo git clone http://tiny.cc/shgi9x
+sudo git clone https://github.com/SteffeyDev/ogp.git
 cd ~/ogp/utilities
 sudo chmod +x install.bash
 sudo ./install.bash
--- utilities/install.bash
+++ utilities/install.bash
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-
+echo "* Getting Updates...\n"
 sudo apt-get update
 sudo easy_install -U distribute
 
+echo "* Installing needed python libraries...\n"
 sudo apt-get install ipython python-serial python-opencv python-picamera python-scipy python-numpy python-setuptools python-pip -y
+
+echo "* Installing NGINX web server and arduino libraries...\n"
 sudo apt-get install nginx arduino -y
 
+echo "* Installing SimpleCV, tornado, and GPIO connections...\n"
 sudo pip install https://github.com/sightmachine/SimpleCV/zipball/master
 sudo pip install tornado
 sudo pip install RPi.GPIO
 
-sudo mv -r ../www /var/www
+sudo rm -rf /var/www
+sudo mv ../www /var/www
+
+echo "* Testing pi camera..."
+raspistill -v -o test.jpg
+
+echo "* If you did not see the image, please run 'sudo raspi-config' and enable the camera module."
+
+cd ~
+touch start_ogp
+echo '#!/bin/bash\nsudo python ~/ogp/python/newsocket.py' > /usr/bin/start_ogp
+chmod +x start_ogp
+sudo mv start_ogp /usr/bin/
+
+echo "* Type the command start_ogp to start the backend server"
