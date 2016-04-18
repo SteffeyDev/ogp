0806e37ee923e44ac9e53ce81a338640b9b92a30
Peter Steffey <petersteffey@MasterPeters-Mac-mini.local>
2016-04-18 11:36:14 -0400

    updates

--- utilities/install.bash
+++ utilities/install.bash
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-echo "* Getting Updates..."
+printf "* Getting Updates...\n"
 # sudo apt-get update
 sudo easy_install -U distribute
 
-echo "\n* Installing needed python libraries..."
-sudo apt-get install ipython python-serial python-opencv python-picamera python-scipy python-numpy python-setuptools python-pip -y
+printf "\n* Installing needed python libraries...\n"
+printf "\n* Installing NGINX web server and arduino libraries...\n"
+sudo apt-get install vsftpd nginx arduino ipython python-serial python-opencv python-picamera python-scipy python-numpy python-setuptools python-pip -y
 
-echo "\n* Installing NGINX web server and arduino libraries..."
-sudo apt-get install nginx arduino -y
-
-echo "\n* Installing SimpleCV, tornado, and GPIO connections..."
+printf "\n* Installing SimpleCV, tornado, and GPIO connections...\n"
 sudo pip install https://github.com/sightmachine/SimpleCV/zipball/master
 sudo pip install tornado
 sudo pip install RPi.GPIO
 sudo pip install svgwrite
 
-sudo mkdir -p /var/www/
-sudo mkdir -p /var/www/images
+sudo rm -rf /var/www/
 sudo cp -R ../www /var/www
+sudo mkdir -p /var/www/images
 
-echo "\n* Testing pi camera..."
+printf "\n* Testing pi camera...\n"
 raspistill -v -o test.jpg
 
-echo "\n* If you did not see the image, please run 'sudo raspi-config' and enable the camera module."
+printf "\n* If you did not see the image, please run 'sudo raspi-config' and enable the camera module."
 
 sudo cp start_ogp /usr/bin/start_ogp
 sudo chmod +x /usr/bin/start_ogp
@@ -31,4 +29,4 @@ sudo chmod +x /usr/bin/start_ogp
 sudo cp update /usr/bin/update
 sudo chmod +x /usr/bin/update
 
-echo "\n* Type the command start_ogp to start the backend server"
+printf "\n* Type the command start_ogp to start the backend server\n"
